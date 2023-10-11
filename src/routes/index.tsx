276fd642108b8bf0b49baadc31bece5fec846555
lukaspkrr <lucas10.mf@gmail.com>
2023-10-11 17:10:28 -0300

    Add vector icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Pokedex} from '../pages';
+import {Pokedex, Moves} from '../pages';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 const Routes = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Pokedex" component={Pokedex} />
+    <Tab.Screen
+      name="Pokedex"
+      component={Pokedex}
+      options={{
+        tabBarLabelStyle: {
+          color: '#000',
+        },
+        tabBarIcon: () => (
+          <Icon name="catching-pokemon" color="#000" size={24} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Moves"
+      component={Moves}
+      options={{
+        tabBarLabelStyle: {
+          color: '#000',
+        },
+        tabBarIcon: () => <Icon name="thunderstorm" color="#000" size={24} />,
+      }}
+    />
     {/* Moves
         Abilities
         Items
